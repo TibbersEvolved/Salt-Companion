b9b8160f2d787edce485fe22c75f4114df82553a
TibbersEvolved <erik.olin.spanga@gmail.com>
2024-11-25 15:45:27 +0100

    Added BootcampService
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>

---
+++ backend/SaltCompanion/src/main/java/chilis/dev/SaltCompanion/services/BootcampService.java
@@ -0,0 +1,9 @@
+package chilis.dev.SaltCompanion.services;
+
+import chilis.dev.SaltCompanion.models.BootCamp;
+
+import java.util.List;
+
+public class BootcampService {
+    private List<BootCamp> bootCampList;
+}
