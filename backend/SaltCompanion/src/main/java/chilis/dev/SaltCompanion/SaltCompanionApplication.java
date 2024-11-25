3578b2dbd2b6dc468540cac67d8da2f8ad227a35
AndreasErikLundmark <andreas.erik.lundmark@gmail.com>
2024-11-25 15:16:20 +0100

    add backend
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Erik Olin <erik.olin.spanga@gmail.com>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>

---
+++ backend/SaltCompanion/src/main/java/chilis/dev/SaltCompanion/SaltCompanionApplication.java
@@ -0,0 +1,13 @@
+package chilis.dev.SaltCompanion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaltCompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaltCompanionApplication.class, args);
+	}
+
+}
---
+++ backend/SaltCompanion/src/test/java/chilis/dev/SaltCompanion/SaltCompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package chilis.dev.SaltCompanion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaltCompanionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
