da5ffe4309c8c482d9262fed0ec933738d76c4cc
Sabine <sabinehernandes@gmail.com>
2024-11-26 09:52:33 +0100

    front - set up login page git commit -m “$1
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>
    Co-authored-by: TibbersEvolved <erik.olin.spanga@gmail.com>

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -8,9 +8,22 @@ export const Route = createFileRoute("/")({
 
 function HomeComponent() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
-      <Login />
-    </div>
+    <>
+      <div className="grid grid-cols-2 min-h-screen">
+        <div className="h-screen">
+          <img
+            src="public/salt.webp"
+            alt="SALT logo"
+            className="object-cover h-full w-full"
+          />
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-4xl p-2 text-center mb-10">
+            Welcome to the ultimate SALT Companion
+          </h1>
+          <Login />
+        </div>
+      </div>
+    </>
   );
 }
