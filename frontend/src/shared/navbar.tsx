53e51376050318064bcb5c02ace2d9644d1580d9
Sabine <sabinehernandes@gmail.com>
2024-11-26 14:10:56 +0100

    front - style and configure navbar git commit -m “$1
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>
    Co-authored-by: TibbersEvolved <erik.olin.spanga@gmail.com>

--- frontend/src/shared/navbar.tsx
+++ frontend/src/shared/navbar.tsx
@@ -1,45 +1,55 @@
+import { SignedIn, UserButton } from "@clerk/clerk-react";
+import { Link } from "@tanstack/react-router";
+import { useState } from "react";
+
 export default function Navbar() {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
   return (
-    <>
-      <div className="navbar bg-base-100">
-        <div className="flex-none">
-          <button className="btn btn-square btn-ghost">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block h-5 w-5 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              ></path>
-            </svg>
-          </button>
-        </div>
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
-        </div>
-        <div className="flex-none">
-          <button className="btn btn-square btn-ghost">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block h-5 w-5 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-              ></path>
-            </svg>
-          </button>
-        </div>
+    <div className="navbar bg-base-100">
+      <div className="flex-1 ml-4">
+        <Link to="/landing">
+          <img src="public/salt-logo-dark.svg" alt="salt logo" />
+        </Link>
       </div>
-    </>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1 mx-4">
+          <li>
+            <details className="mx-4">
+              <summary>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="inline-block h-5 w-5 stroke-current"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  ></path>
+                </svg>
+              </summary>
+              <ul className="bg-base-100 rounded-t-none p-2">
+                <li>
+                  <a>Link1</a>
+                </li>
+                <li>
+                  <a>Link2</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </ul>
+      </div>
+    </div>
   );
 }
