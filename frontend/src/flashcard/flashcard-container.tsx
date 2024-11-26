0c4b7aa28b98233991235a6d82cacf386eb72e64
Sabine <sabinehernandes@gmail.com>
2024-11-26 16:28:30 +0100

    front - create flashcard git commit -m “$1
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>
    Co-authored-by: TibbersEvolved <erik.olin.spanga@gmail.com>

--- frontend/src/flashcard/flashcard-container.tsx
+++ frontend/src/flashcard/flashcard-container.tsx
@@ -1,7 +1,18 @@
 export const Flashcard = () => {
   return (
     <>
-      <h1>Flashcards</h1>
+      <h1 className="text-3xl text-center mt-10">flashcard.title</h1>
+      <div className="flex justify-center mt-10">
+        <div className="w-[600px] h-96 bg-gray-200 flex items-center justify-center">
+          <p>flashcard.front</p>
+          <div>
+            <button>Very hard</button>
+            <button>Hard</button>
+            <button>Easy</button>
+            <button>Too easy</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
