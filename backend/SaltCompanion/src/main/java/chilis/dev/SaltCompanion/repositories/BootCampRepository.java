54bd3583524e01f478c8dac971915bf630b0272c
TibbersEvolved <erik.olin.spanga@gmail.com>
2024-11-26 10:37:02 +0100

    Added service with prepoulate stuff
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>

--- backend/SaltCompanion/src/main/java/chilis/dev/SaltCompanion/repositories/BootCampRepository.java
+++ backend/SaltCompanion/src/main/java/chilis/dev/SaltCompanion/repositories/BootCampRepository.java
@@ -2,6 +2,8 @@ package chilis.dev.SaltCompanion.repositories;
 
 import chilis.dev.SaltCompanion.models.BootCamp;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BootCampRepository extends ListCrudRepository<BootCamp, Long> {
 }
--- backend/SaltCompanion/src/main/java/chilis/dev/SaltCompanion/services/BootcampService.java
+++ backend/SaltCompanion/src/main/java/chilis/dev/SaltCompanion/services/BootcampService.java
@@ -1,6 +1,9 @@
 package chilis.dev.SaltCompanion.services;
 
 import chilis.dev.SaltCompanion.models.BootCamp;
+import chilis.dev.SaltCompanion.models.Teacher;
+import chilis.dev.SaltCompanion.models.Topic;
+import chilis.dev.SaltCompanion.repositories.BootCampRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -8,12 +11,34 @@ import java.util.List;
 
 @Service
 public class BootcampService {
-    private List<BootCamp> bootCampList = new ArrayList<>();
+
+    BootCampRepository bootCampRepository;
+
+    public BootcampService(BootCampRepository bootCampRepository) {
+        this.bootCampRepository = bootCampRepository;
+        Teacher alek = new Teacher("Alek", "alek@saltEmail.com");
+        Long id = addBootCamp("JFS",alek);
+        Topic topic = new Topic("Java");
+        addTopicToBootCamp(id,topic);
+    }
+
+    public Long addBootCamp(String name, Teacher teacher) {
+        BootCamp bootCamp = new BootCamp(name, teacher);
+        bootCampRepository.save(bootCamp);
+        return bootCamp.getId();
+    }
+
+    public void addTopicToBootCamp(Long id, Topic topic) {
+        BootCamp bootCamp = bootCampRepository.findById(id).get();
+        bootCamp.addTopic(topic);
+        bootCampRepository.save(bootCamp);
+    }
 
     public BootCamp getBootCamp(long id) {
         //Fetches a bootcamp by id.
         return null;
     }
-    //Some methods to add and remove cards to Bootcamps.
+
+
 
 }
