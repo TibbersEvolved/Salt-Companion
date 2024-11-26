9ca5988269110866449269b9281ef3e887006ee8
John Eliasson <john.eliasson@appliedtechnology.se>
2024-11-26 13:18:46 +0100

    Config tailwind
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>
    Co-authored-by: TibbersEvolved <erik.olin.spanga@gmail.com>

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
 export default {
   content: ["./src/**/*.{html,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
 };
