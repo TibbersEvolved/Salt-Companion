fa8215d98917af9a7623974ca0cc54ed67226f29
Sabine <sabinehernandes@gmail.com>
2024-11-26 09:56:23 +0100

    front - remove navbar from root

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -17,26 +17,6 @@ function RootComponent() {
   return (
     <>
       <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-        <div className="p-2 flex gap-2 text-lg">
-          <Link
-            to="/"
-            activeProps={{
-              className: "font-bold",
-            }}
-            activeOptions={{ exact: true }}
-          >
-            Home
-          </Link>{" "}
-          <Link
-            to="/about"
-            activeProps={{
-              className: "font-bold",
-            }}
-          >
-            About
-          </Link>
-        </div>
-        <hr />
         <Outlet />
         <TanStackRouterDevtools position="bottom-right" />
       </ClerkProvider>
