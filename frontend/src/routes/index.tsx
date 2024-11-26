67385c46326935a3cf45edbe410762cc452d88a3
Sabine <sabinehernandes@gmail.com>
2024-11-26 10:35:11 +0100

    change loginpage function nme

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -3,10 +3,10 @@ import { createFileRoute } from "@tanstack/react-router";
 import Login from "../login/login-component";
 
 export const Route = createFileRoute("/")({
-  component: HomeComponent,
+  component: LoginPage,
 });
 
-function HomeComponent() {
+function LoginPage() {
   return (
     <>
       <div className="grid grid-cols-2 min-h-screen">
