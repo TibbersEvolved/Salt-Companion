800ffdfe07a385ec9bf50d9646176075265ad8b6
TibbersEvolved <erik.olin.spanga@gmail.com>
2024-11-27 10:12:27 +0100

    Started on student service
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>

--- backend/SaltCompanion/src/main/java/chilis/dev/SaltCompanion/services/StudentService.java
+++ backend/SaltCompanion/src/main/java/chilis/dev/SaltCompanion/services/StudentService.java
@@ -1,5 +1,6 @@
 package chilis.dev.SaltCompanion.services;
 
+import chilis.dev.SaltCompanion.controllers.BootcampController;
 import chilis.dev.SaltCompanion.models.BootCamp;
 import chilis.dev.SaltCompanion.repositories.StudentRepo;
 import org.springframework.stereotype.Service;
@@ -8,13 +9,16 @@ import org.springframework.stereotype.Service;
 public class StudentService {
 
     StudentRepo studentRepo;
+    BootcampController bootcampController;
 
-    public StudentService(StudentRepo studentRepo) {
+    public StudentService(StudentRepo studentRepo, BootcampController bootcampController) {
         this.studentRepo = studentRepo;
+        this.bootcampController = bootcampController;
     }
 
     public void createStudent(String name, String clerkId, BootCamp bootCamp) {
-
     }
+
+
 
 }
