a7a5ad4f6f2ec6c308a80519f01de34f0d4a9164
John Eliasson <john.eliasson@appliedtechnology.se>
2024-11-27 09:35:58 +0100

    add functionality to check the value of select form
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>
    Co-authored-by: TibbersEvolved <erik.olin.spanga@gmail.com>

--- frontend/src/landing-page/landing-page.tsx
+++ frontend/src/landing-page/landing-page.tsx
@@ -2,9 +2,16 @@ import { SignedIn, UserProfile, useUser } from "@clerk/clerk-react";
 import { Link } from "@tanstack/react-router";
 import { mockedCourseData } from "../mocked/mocked-data";
 import Select from "react-select";
+import { useState } from "react";
 
 export const LandingPage = () => {
   const { user } = useUser();
+  const [selectedOptions, setSelectedOptions] = useState([]);
+
+  const handleChange = (selected) => {
+    setSelectedOptions(selected);
+    console.log(selected);
+  };
 
   return (
     <div className="m-0 p-0 overflow-hidden place-content-center">
@@ -21,14 +28,18 @@ export const LandingPage = () => {
               options={mockedCourseData}
               className="text-black w-1/2"
               classNamePrefix="select"
+              onChange={handleChange}
             />
           </div>
           <button className="text-5xl border-solid rounded-lg border-2 border-black w-fit">
             Next
           </button>
         </div>
-        <div className="w-screen h-screen overflow-hidden bg-gray-200">
+        <div className="w-screen h-screen overflow-hidden bg-white grid  ">
           <h2 className="text-center text-5xl">Stats</h2>
+          <div className="border border-black h-10/12 w-3/4">
+            <h2>buncha scool stats</h2>
+          </div>
         </div>
       </SignedIn>
     </div>
