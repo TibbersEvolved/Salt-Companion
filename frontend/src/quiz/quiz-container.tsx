58150098aceaa7c3f29b5170185147e82845ce3f
John Eliasson <john.eliasson@appliedtechnology.se>
2024-11-27 13:28:18 +0100

    Add styling to quizz
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>
    Co-authored-by: TibbersEvolved <erik.olin.spanga@gmail.com>

--- frontend/src/quiz/quiz-container.tsx
+++ frontend/src/quiz/quiz-container.tsx
@@ -1,7 +1,27 @@
+import { SignedIn } from "@clerk/clerk-react";
+
 export const Quiz = () => {
   return (
-    <>
-      <h1>Quiz</h1>
-    </>
+    <SignedIn>
+      <div className="m-0 p-0 bg-white w-screen h-screen flex justify-center items-center">
+        <div className="border border-black bg-slate-200 h-3/4 w-3/4 flex justify-evenly flex-col items-center round shadow-lg">
+          <h4 className="text-3xl text-black">Question</h4>
+          <div className="flex flex-col w-full items-center">
+            <button className="m-2 p-4 w-10/12 min-h-7 shadow-md bg-gray-400 text-black border border-black hover:bg-gray-500 transition duration-200">
+              answer 1
+            </button>
+            <button className="m-2 p-4 w-10/12 min-h-7 shadow-md bg-gray-400 text-black border border-black hover:bg-gray-500 transition duration-200">
+              answer 2
+            </button>
+            <button className="m-2 p-4 w-10/12 min-h-7 shadow-md bg-gray-400 text-black border border-black hover:bg-gray-500 transition duration-200">
+              answer 3
+            </button>
+            <button className="m-2 p-4 w-10/12 min-h-7 shadow-md bg-gray-400 text-black border border-black hover:bg-gray-500 transition duration-200">
+              answer 4
+            </button>
+          </div>
+        </div>
+      </div>
+    </SignedIn>
   );
 };
