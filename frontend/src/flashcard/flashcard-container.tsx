972f8686eca279fd1ae915a712ffe9ff4b8d34b8
Sabine <sabinehernandes@gmail.com>
2024-11-27 10:05:21 +0100

    front - style flashcard buttons and background color git commit -m “$1
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>
    Co-authored-by: TibbersEvolved <erik.olin.spanga@gmail.com>

--- frontend/src/flashcard/flashcard-container.tsx
+++ frontend/src/flashcard/flashcard-container.tsx
@@ -3,13 +3,19 @@ export const Flashcard = () => {
     <>
       <h1 className="text-3xl text-center mt-10">flashcard.title</h1>
       <div className="flex justify-center mt-10">
-        <div className="w-[600px] h-96 bg-gray-200 flex items-center justify-center">
+        <div className="w-[600px] h-96 bg-stone-800/5 flex items-center justify-center relative rounded-lg">
           <p>flashcard.front</p>
-          <div>
-            <button>Difficult</button>
-            <button>Moderate</button>
-            <button>Easy</button>
-            <button>Very easy</button>
+          <div className="absolute bottom-6 space-x-3 text-white">
+            <button className="bg-red-800 w-24 p-2 rounded-md">
+              Difficult
+            </button>
+            <button className="bg-yellow-500 w-24 p-2 rounded-md">
+              Moderate
+            </button>
+            <button className="bg-sky-600 w-24 p-2 rounded-md">Easy</button>
+            <button className="bg-green-700 w-24 p-2 rounded-md">
+              Very easy
+            </button>
           </div>
         </div>
       </div>
