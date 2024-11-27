618a93b239947260108047034123c98b9d8cf319
Sabine <sabinehernandes@gmail.com>
2024-11-27 14:17:53 +0100

    front - render flashcard information from mocked data and update button functionality to go to next card when clicked git commit -m “$1
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>
    Co-authored-by: TibbersEvolved <erik.olin.spanga@gmail.com>

--- frontend/src/flashcard/flashcard-container.tsx
+++ frontend/src/flashcard/flashcard-container.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactFlipCard from "reactjs-flip-card";
+import { flashCard } from "../mocked/mocked-data";
 
 export const Flashcard = () => {
+  const [currentCardIndex, setCurrentCardIndex] = useState(0);
+  const currentCard = flashCard[currentCardIndex];
+
+  const nextCard = () => {
+    setCurrentCardIndex((prevIndex) =>
+      prevIndex < flashCard.length - 1 ? prevIndex + 1 : 0
+    );
+  };
+
   return (
     <>
-      <h1 className="text-3xl text-center mt-10">flashcard.title</h1>
+      <h1 className="text-3xl text-center mt-10">{currentCard.question}</h1>
 
       <div className="flex justify-center mt-16 space-y-36 bg-">
         <ReactFlipCard
@@ -18,25 +28,40 @@ export const Flashcard = () => {
           backComponent={
             <>
               <div className="flex flex-col  bg-red-300 justify-between h-full items-center rounded-lg">
-                <div className="flex-grow flex items-center justify-center">
-                  <p>Here's your answer</p>
+                <div className="flex-grow flex items-center justify-center p-12 text-center text-2xl">
+                  <p>{currentCard.answer}</p>
                 </div>
                 <div className="absolute bottom-6 space-x-3 text-white">
-                  <button className="bg-red-800 w-24 p-2 rounded-md">
+                  <button
+                    className="bg-red-800 w-24 p-2 rounded-md"
+                    onClick={() => nextCard()}
+                  >
                     Difficult
                   </button>
-                  <button className="bg-yellow-500 w-24 p-2 rounded-md">
+                  <button
+                    className="bg-yellow-500 w-24 p-2 rounded-md"
+                    onClick={() => nextCard()}
+                  >
                     Moderate
                   </button>
-                  <button className="bg-sky-600 w-24 p-2 rounded-md">
+                  <button
+                    className="bg-sky-600 w-24 p-2 rounded-md"
+                    onClick={() => nextCard()}
+                  >
                     Easy
                   </button>
-                  <button className="bg-green-700 w-24 p-2 rounded-md">
+                  <button
+                    className="bg-green-700 w-24 p-2 rounded-md"
+                    onClick={() => nextCard()}
+                  >
                     Very easy
                   </button>
                 </div>
               </div>
-              <p className="text-center mt-8 italic text-gray-500">
+              <p
+                className="text-center mt-8 italic text-gray-500"
+                onClick={() => nextCard()}
+              >
                 How difficult was this card? Select an option to continue
               </p>
             </>
