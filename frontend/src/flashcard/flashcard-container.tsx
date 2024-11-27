fd1d2ccdc4b3aa9a186ec595d6f6ab94718d085a
Sabine <sabinehernandes@gmail.com>
2024-11-27 11:50:39 +0100

    front - create and style flashcard component git commit -m “$1
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>
    Co-authored-by: TibbersEvolved <erik.olin.spanga@gmail.com>

--- frontend/src/flashcard/flashcard-container.tsx
+++ frontend/src/flashcard/flashcard-container.tsx
@@ -1,23 +1,44 @@
+import React from "react";
+import ReactFlipCard from "reactjs-flip-card";
+
 export const Flashcard = () => {
   return (
     <>
       <h1 className="text-3xl text-center mt-10">flashcard.title</h1>
-      <div className="flex justify-center mt-10">
-        <div className="w-[600px] h-96 bg-stone-800/5 flex items-center justify-center relative rounded-lg">
-          <p>flashcard.front</p>
-          <div className="absolute bottom-6 space-x-3 text-white">
-            <button className="bg-red-800 w-24 p-2 rounded-md">
-              Difficult
-            </button>
-            <button className="bg-yellow-500 w-24 p-2 rounded-md">
-              Moderate
-            </button>
-            <button className="bg-sky-600 w-24 p-2 rounded-md">Easy</button>
-            <button className="bg-green-700 w-24 p-2 rounded-md">
-              Very easy
-            </button>
-          </div>
-        </div>
+
+      <div className="flex justify-center mt-16 space-y-36 bg-">
+        <ReactFlipCard
+          flipTrigger="onClick"
+          containerCss="w-[600px] h-96 bg-sky-300 flex items-center justify-center relative rounded-lg"
+          frontComponent={
+            <div className="flex items-center justify-center h-full">
+              <p>Click to see the answer</p>
+            </div>
+          }
+          backComponent={
+            <>
+              <div className="flex flex-col  bg-red-300 justify-between h-full items-center rounded-lg">
+                <div className="flex-grow flex items-center justify-center">
+                  <p>Here's your answer</p>
+                </div>
+                <div className="absolute bottom-6 space-x-3 text-white">
+                  <button className="bg-red-800 w-24 p-2 rounded-md">
+                    Difficult
+                  </button>
+                  <button className="bg-yellow-500 w-24 p-2 rounded-md">
+                    Moderate
+                  </button>
+                  <button className="bg-sky-600 w-24 p-2 rounded-md">
+                    Easy
+                  </button>
+                  <button className="bg-green-700 w-24 p-2 rounded-md">
+                    Very easy
+                  </button>
+                </div>
+              </div>
+            </>
+          }
+        />
       </div>
     </>
   );
