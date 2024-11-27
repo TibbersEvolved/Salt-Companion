183278b146a58b3e3c29a932c1b94ba195b5b6b2
AndreasErikLundmark <andreas.erik.lundmark@gmail.com>
2024-11-27 13:26:21 +0100

    teacer service ok
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Erik Olin <erik.olin.spanga@gmail.com>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>

--- backend/SaltCompanion/src/main/java/chilis/dev/SaltCompanion/repositories/TeacherRepository.java
+++ backend/SaltCompanion/src/main/java/chilis/dev/SaltCompanion/repositories/TeacherRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TeacherRepository extends ListCrudRepository<Teacher, Long> {
+    boolean findByClerkId(String clerkId);
+
+    Teacher findTeacherByClerkId(String clerkId);
 }
--- backend/SaltCompanion/src/main/java/chilis/dev/SaltCompanion/services/TeacherService.java
+++ backend/SaltCompanion/src/main/java/chilis/dev/SaltCompanion/services/TeacherService.java
@@ -11,8 +11,25 @@ public class TeacherService {
         this.teacherRepository = teacherRepository;
     }
 
-    public void createTeacher(String name) {
-        teacherRepository.save(new Teacher(name));
+    public void createTeacher(String clerkId, String name, String email) {
+        if(teacherRepository.findByClerkId(clerkId)) {
+            throw new IllegalArgumentException("Teacher with clerkId " + clerkId + " already exists");
+        }
+        teacherRepository.save(new Teacher(clerkId,name,email));
     }
+
+    public Teacher findTeacherByClerkId(String clerkId) {
+        return teacherRepository.findTeacherByClerkId(clerkId);
+    }
+    public boolean deleteTeacher(String clerkId){
+        Teacher teacher = teacherRepository.findTeacherByClerkId(clerkId);
+        if(teacher == null) {
+            throw new NullPointerException("Teacher: " + clerkId + " does not exist. Cannot delete");
+        }
+        teacherRepository.delete(teacher);
+        return true;
+    }
+
+
 
 }
