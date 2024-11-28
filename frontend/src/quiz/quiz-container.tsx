e66105437600067c3fec4a9c1ffdce59adae1277
Sabine <sabinehernandes@gmail.com>
2024-11-28 17:36:38 +0100

    front - style quiz-container from scratch git commit -m “$1
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>
    Co-authored-by: TibbersEvolved <erik.olin.spanga@gmail.com>

--- frontend/src/quiz/quiz-container.tsx
+++ frontend/src/quiz/quiz-container.tsx
@@ -3,31 +3,57 @@ import { mockedQuiz } from "../mocked/mocked-data";
 
 export const Quiz = () => {
   return (
-    <SignedIn>
-      <div className="m-0 p-0 bg-white w-screen h-screen flex justify-center items-center">
-        {mockedQuiz.map((q) => (
-          <div className="border border-black bg-slate-200 h-3/4 w-3/4 flex justify-evenly flex-col items-center round shadow-lg">
-            <h4 className="text-3xl text-black">{q.question}</h4>
-            <div className="flex flex-col w-full items-center">
-              <button className="m-2 p-4 w-10/12 min-h-7 shadow-md rounded-lg bg-gray-400 text-black border border-black hover:bg-gray-500 transition duration-200">
-                {q.answer1}
-              </button>
-              <button className="m-2 p-4 w-10/12 min-h-7 shadow-md rounded-lg bg-gray-400 text-black border border-black hover:bg-gray-500 transition duration-200">
-                {q.answer2}
-              </button>
-              <button className="m-2 p-4 w-10/12 min-h-7 shadow-md rounded-lg bg-gray-400 text-black border border-black hover:bg-gray-500 transition duration-200">
-                {q.answer3}
-              </button>
-              <button className="m-2 p-4 w-10/12 min-h-7 shadow-md rounded-lg bg-gray-400 text-black border border-black hover:bg-gray-500 transition duration-200">
-                {q.answer4}
+    <>
+      <SignedIn>
+        <div className="m-0 p-0 w-screen h-screen flex justify-center items-center">
+          {mockedQuiz.map((q) => (
+            <div className=" bg-white rounded-2xl h-3/4 w-3/4 flex justify-evenly flex-col items-center round shadow-lg">
+              <h2 className="text-4xl">{q.question}</h2>
+              <div className="flex flex-col w-full items-start text-2xl space-y-4">
+                <div className="flex items-center space-x-4 mx-32">
+                  <input
+                    type="radio"
+                    name="answer"
+                    id="answer-1"
+                    className="appearance-none w-5 h-5 border-2 border-gray-500/50 rounded-full"
+                  />
+                  <label htmlFor="answer-1">{q.answer1}</label>
+                </div>
+                <div className="flex items-center space-x-4 mx-32">
+                  <input
+                    type="radio"
+                    name="answer"
+                    id="answer-2"
+                    className="appearance-none w-5 h-5 border-2 border-gray-500/50 rounded-full"
+                  />
+                  <label htmlFor="answer-2">{q.answer2}</label>
+                </div>
+                <div className="flex items-center space-x-4 mx-32">
+                  <input
+                    type="radio"
+                    name="answer"
+                    id="answer-3"
+                    className="appearance-none w-5 h-5 border-2 border-gray-500/50 rounded-full"
+                  />
+                  <label htmlFor="answer-3">{q.answer3}</label>
+                </div>
+                <div className="flex items-center space-x-4 mx-32">
+                  <input
+                    type="radio"
+                    name="answer"
+                    id="answer-4"
+                    className="appearance-none w-5 h-5 border-2 border-gray-500/50 rounded-full"
+                  />
+                  <label htmlFor="answer-4">{q.answer4}</label>
+                </div>
+              </div>
+              <button className="mt-6 bg-[#fc7961] text-white h-10 w-1/5 rounded-full text-lg font-semibold hover:bg-[#f35b7e] transition duration-200">
+                Next
               </button>
             </div>
-            <button className="m-2 p-4 w-4/12 border text-black rounded-lg border-black shadow-md bg-gray-400 hover:bg-gray-500 transition duration-200">
-              Next
-            </button>
-          </div>
-        ))}
-      </div>
-    </SignedIn>
+          ))}
+        </div>
+      </SignedIn>
+    </>
   );
 };
