0f8793e1a377a778f92c3794714ed9471b3a9c22
Sabine <sabinehernandes@gmail.com>
2024-11-28 19:19:03 +0100

    front - add logic to get questions from mocked data and display one at a time and text to display when user finishes the quiz git commit -m “$1
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>
    Co-authored-by: TibbersEvolved <erik.olin.spanga@gmail.com>

--- frontend/src/quiz/quiz-container.tsx
+++ frontend/src/quiz/quiz-container.tsx
@@ -1,14 +1,38 @@
 import { SignedIn } from "@clerk/clerk-react";
 import { mockedQuiz } from "../mocked/mocked-data";
+import { useState } from "react";
+import { Link } from "@tanstack/react-router";
 
 export const Quiz = () => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const currentQuestion = mockedQuiz[currentQuestionIndex];
+
+  const nextQuestion = () => {
+    setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+  };
+
+  const isEndReached = currentQuestionIndex >= mockedQuiz.length;
+
   return (
     <>
       <SignedIn>
-        <div className="m-0 p-0 w-screen h-screen flex justify-center items-center">
-          {mockedQuiz.map((q) => (
+        {isEndReached ? (
+          <>
+            <div className="flex flex-col items-center justify-center h-screen">
+              <h1 className="text-3xl text-center mt-10">
+                Thanks for submitting!
+              </h1>
+              <Link to="/landing">
+                <button className="bg-rose-600 text-white px-6 py-3 rounded-md mt-6">
+                  Go back to the main page
+                </button>
+              </Link>
+            </div>
+          </>
+        ) : (
+          <div className="m-0 p-0 w-screen h-screen flex justify-center items-center">
             <div className=" bg-white rounded-2xl h-3/4 w-3/4 flex justify-evenly flex-col items-center round shadow-lg">
-              <h2 className="text-4xl">{q.question}</h2>
+              <h2 className="text-4xl">{currentQuestion.question}</h2>
               <div className="flex flex-col w-full items-start text-2xl space-y-4">
                 <div className="flex items-center space-x-4 mx-32">
                   <input
@@ -17,7 +41,7 @@ export const Quiz = () => {
                     id="answer-1"
                     className="appearance-none w-5 h-5 border-2 border-gray-500/50 rounded-full"
                   />
-                  <label htmlFor="answer-1">{q.answer1}</label>
+                  <label htmlFor="answer-1">{currentQuestion.answer1}</label>
                 </div>
                 <div className="flex items-center space-x-4 mx-32">
                   <input
@@ -26,7 +50,7 @@ export const Quiz = () => {
                     id="answer-2"
                     className="appearance-none w-5 h-5 border-2 border-gray-500/50 rounded-full"
                   />
-                  <label htmlFor="answer-2">{q.answer2}</label>
+                  <label htmlFor="answer-2">{currentQuestion.answer2}</label>
                 </div>
                 <div className="flex items-center space-x-4 mx-32">
                   <input
@@ -35,7 +59,7 @@ export const Quiz = () => {
                     id="answer-3"
                     className="appearance-none w-5 h-5 border-2 border-gray-500/50 rounded-full"
                   />
-                  <label htmlFor="answer-3">{q.answer3}</label>
+                  <label htmlFor="answer-3">{currentQuestion.answer3}</label>
                 </div>
                 <div className="flex items-center space-x-4 mx-32">
                   <input
@@ -44,15 +68,18 @@ export const Quiz = () => {
                     id="answer-4"
                     className="appearance-none w-5 h-5 border-2 border-gray-500/50 rounded-full"
                   />
-                  <label htmlFor="answer-4">{q.answer4}</label>
+                  <label htmlFor="answer-4">{currentQuestion.answer4}</label>
                 </div>
               </div>
-              <button className="mt-6 bg-[#fc7961] text-white h-10 w-1/5 rounded-full text-lg font-semibold hover:bg-[#f35b7e] transition duration-200">
+              <button
+                onClick={nextQuestion}
+                className="mt-6 bg-[#fc7961] text-white h-10 w-1/5 rounded-full text-lg font-semibold hover:bg-[#f35b7e] transition duration-200"
+              >
                 Next
               </button>
             </div>
-          ))}
-        </div>
+          </div>
+        )}
       </SignedIn>
     </>
   );
