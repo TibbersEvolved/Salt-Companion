5e5ade4b18158d8dc0caf736924f9c4b7905ce2f
AndreasErikLundmark <andreas.erik.lundmark@gmail.com>
2024-11-28 14:13:24 +0100

    woodo
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Erik Olin <erik.olin.spanga@gmail.com>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>

--- backend/SaltCompanion/src/main/java/chilis/dev/SaltCompanion/services/ZpplicationInitializer.java
+++ backend/SaltCompanion/src/main/java/chilis/dev/SaltCompanion/services/ZpplicationInitializer.java
@@ -1,16 +1,15 @@
 package chilis.dev.SaltCompanion.services;
 
-import chilis.dev.SaltCompanion.models.BootCamp;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class ApplicationInitializer {
+public class ZpplicationInitializer {
 
     private final TableBootService tableBootService;
 
-    public ApplicationInitializer(TableBootService tableBootService, BootcampService bootcampService) {
+    public ZpplicationInitializer(TableBootService tableBootService, BootcampService bootcampService) {
         this.tableBootService = tableBootService;
     }
 
