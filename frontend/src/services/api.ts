60de9dde4a43fdea81883118fd641fbba16b2bfd
Sabine <sabinehernandes@gmail.com>
2024-11-29 15:59:18 +0100

    front - add fetchBootcamp fetch and fix startFlashcardSession git commit -m “$1
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>
    Co-authored-by: TibbersEvolved <erik.olin.spanga@gmail.com>

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -12,16 +12,13 @@ export const startFlashcardSession = async (
     topicIdList,
     userId,
   };
+  console.log(JSON.stringify(payload));
   try {
-    const response = await axios.post(
-      `${base_url}/flashSession`,
-      JSON.stringify(payload),
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    const response = await axios.post(`${base_url}/flashSession`, payload, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     return response.data;
   } catch (error) {
     console.log("Error fetching flashcard questions. Please try again later");
@@ -30,9 +27,29 @@ export const startFlashcardSession = async (
 
 export const getQuizQuestions = async () => {
   try {
-    const response = await axios.get(`${base_url}/${"SOME_ENDPOINT"}`);
+    const response = await axios.get(`${base_url}/bootcamps`);
+    console.log(response.data);
     return response.data;
   } catch (error) {
     console.log("Error fetching quiz questions. Please try again later");
+  }
+};
+
+export const fetchBootcamps = async () => {
+  const payload = {
+    name: "Sabine",
+    clerkId: "test",
+  };
+  try {
+    const response = fetch(`${base_url}/bootcamps`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify(payload),
+    });
+    return response;
+  } catch (error) {
+    console.log("Error fetching bootcamps. Please try again later");
   }
 };
