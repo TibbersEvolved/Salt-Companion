a056fa762e342dd5feeed1c0ee3ef779566274f7
Sabine <sabinehernandes@gmail.com>
2024-11-29 16:24:04 +0100

    front - properly fetch and refetch from backend git commit -m “$1
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>
    Co-authored-by: TibbersEvolved <erik.olin.spanga@gmail.com>

--- frontend/src/flashcard/flashcard-container.tsx
+++ frontend/src/flashcard/flashcard-container.tsx
@@ -4,38 +4,43 @@ import { flashCard } from "../mocked/mocked-data";
 import { Link } from "@tanstack/react-router";
 import Confetti from "react-confetti";
 import { useWindowSize } from "react-use";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getFlashcardQuestions } from "../services/api";
 
 type FlashcardProps = {
   sessionId: string;
 };
 
+type FlashcardQuestionsAndAnswers = {
+  topic: string;
+  question: string;
+  answer: string;
+  cardLeft: number;
+};
+
 export const Flashcard = (prop: FlashcardProps) => {
   const [currentCardIndex, setCurrentCardIndex] = useState(0);
-  const currentCard = flashCard[currentCardIndex];
   const { width, height } = useWindowSize();
+  const client = useQueryClient();
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["flashCards"],
+    queryFn: () => getFlashcardQuestions(prop.sessionId),
+  });
 
   const nextCard = () => {
     setCurrentCardIndex((prevIndex) => prevIndex + 1);
+    client.invalidateQueries({
+      queryKey: ["flashCards"],
+      refetchType: "all",
+    });
   };
 
-  const isEndReached = currentCardIndex >= flashCard.length;
+  if (isLoading) return <div>Loading pls add toast {prop.sessionId}</div>;
+  if (isError) return <div>Error pls add toast</div>;
 
-  // const { data: flashCards, isLoading, isError } = useQuery({
-  //   queryKey: ["flashCards"],
-  //   queryFn: getFlashcardQuestions,
-  // });
-
-  // const nextCard = () => {
-  //   setCurrentCardIndex((prevIndex) => prevIndex + 1);
-  // }
-
-  // const isEndReached = currentCardIndex >= flashCards.length;
-  // const currentCard = flashCards[currentCardIndex];
-
-  // if (isLoading) return <div>Loading pls add toast</div>
-  // if (isError) return <div>Error pls add toas</div>
+  const typeData: FlashcardQuestionsAndAnswers = data;
+  const isEndReached = 0 >= typeData.cardLeft;
 
   return (
     <>
@@ -61,10 +66,8 @@ export const Flashcard = (prop: FlashcardProps) => {
           <div className="flex items-center justify-center h-screen">
             <div className="bg-[#eed1d6] h-[670px] w-[750px] rounded-2xl">
               <div className="text-center space-y-5 mt-5">
-                <p className="text-[#f36384]">TOPIC NAME</p>
-                <h2 className="text-3xl font-extrabold">
-                  {currentCard.question}
-                </h2>
+                <p className="text-[#f36384]">{typeData.topic}</p>
+                <h2 className="text-3xl font-extrabold">{typeData.question}</h2>
                 {/* {currentCard && (
                 <h2 className="text-3xl font-extrabold">{currentCard.question}</h2>
               )} */}
@@ -78,6 +81,7 @@ export const Flashcard = (prop: FlashcardProps) => {
                     <div className="flex items-center justify-center h-full">
                       <p className="text-2xl text-[#424242]">
                         Click to see the answer
+                        {prop.sessionId}
                       </p>
                     </div>
                   }
@@ -85,7 +89,7 @@ export const Flashcard = (prop: FlashcardProps) => {
                     <>
                       <div className="flex flex-col  bg-[#0f2d45] text-white justify-between h-full items-center rounded-2xl">
                         <div className="flex-grow flex items-center justify-center p-12 text-center text-2xl">
-                          <p className="font-bold">{currentCard.answer}</p>
+                          <p className="font-bold">{typeData.answer}</p>
                         </div>
                       </div>
                       <p
