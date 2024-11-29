ff432a42306645d065bfc43eb5355ba7db196ae4
Sabine <sabinehernandes@gmail.com>
2024-11-29 13:24:52 +0100

    front - create startFlashcardSession post api git commit -m “$1
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>
    Co-authored-by: TibbersEvolved <erik.olin.spanga@gmail.com>

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,9 +2,26 @@ import axios from "axios";
 
 const base_url = import.meta.env.VITE_BASE_URL;
 
-export const getFlashcardQuestions = async () => {
+export const startFlashcardSession = async (
+  cards: number,
+  topicIdList: number[],
+  userId: string
+) => {
+  const payload = {
+    cards,
+    topicIdList,
+    userId,
+  };
   try {
-    const response = await axios.get(`${base_url}/topic`);
+    const response = await axios.post(
+      `${base_url}/flashSession`,
+      JSON.stringify(payload),
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
     return response.data;
   } catch (error) {
     console.log("Error fetching flashcard questions. Please try again later");
