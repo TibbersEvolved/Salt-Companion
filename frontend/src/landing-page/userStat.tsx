ab913de27552da52639f3f33d935c704375972cb
TibbersEvolved <erik.olin.spanga@gmail.com>
2024-12-02 16:20:19 +0100

    Added shadows to stats
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>

--- frontend/src/landing-page/userStat.tsx
+++ frontend/src/landing-page/userStat.tsx
@@ -1,6 +1,6 @@
 export default function UserStat(prop: props) {
     return (<div className="tooltip" data-tip={prop.tooltip}>
-        <button className="btn bg-[#fc7961] text-white h-10   text-md font-semibold hover:bg-[#f35b7e]">{prop.text}</button>
+        <button className="btn bg-[#fc7961] text-white h-10   text-md font-semibold hover:bg-[#f35b7e] shadow-md">{prop.text}</button>
     </div>)
 }
 
