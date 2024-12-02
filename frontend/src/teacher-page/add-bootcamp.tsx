6281f19f13bbc85f7452537a4c992757c3c98e11
AndreasErikLundmark <andreas.erik.lundmark@gmail.com>
2024-12-02 11:50:51 +0100

    create bootcamp
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Erik Olin <erik.olin.spanga@gmail.com>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>

---
+++ frontend/src/teacher-page/add-bootcamp.tsx
@@ -0,0 +1,53 @@
+import { useMutation, UseMutationResult } from "@tanstack/react-query";
+import { createBootCampFetch } from "./fetch-create-bootcamp";
+import { BootCampData } from "./types";
+import { useState, useRef, createContext, useContext, useEffect } from "react";
+
+export function createBootCamp() {
+  const [bootCampName, setBootCampName] = useState<string>("");
+  const [clerkId, setClerkId] = useState<string>("");
+  const [requestBody, setRequestBody] = useState<BootCampData>({
+    name: bootCampName,
+    clerkId: clerkId,
+  });
+
+  const mutationCreateGame: UseMutationResult<string, Error, BootCampData> =
+    useMutation({
+      mutationFn: async (requestBody: BootCampData): Promise<string> => {
+        if (!requestBody) {
+          throw new Error("Request body is required");
+        }
+        return await createBootCampFetch(requestBody);
+      },
+      onSuccess: (data: string) => {
+        if (data) {
+          mutationCreateGame.mutate(requestBody);
+        }
+      },
+      onError: (error) => {
+        console.error("Error fetching the current game state:", error);
+      },
+    });
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={bootCampName}
+        onChange={(e) => setBootCampName(e.target.value)}
+        placeholder="Bootcamp name"
+      />
+      <button
+        onClick={() => {
+          setRequestBody({
+            name: bootCampName,
+            clerkId: clerkId,
+          });
+          mutationCreateGame.mutate(requestBody);
+        }}
+      >
+        Create Bootcamp
+      </button>
+    </div>
+  );
+}
