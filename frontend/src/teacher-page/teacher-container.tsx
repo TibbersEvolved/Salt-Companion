6b4fa57f606ab23ad67d1dda0066556b226d2757
John Eliasson <john.eliasson@appliedtechnology.se>
2024-12-02 15:48:30 +0100

    Add "Wireframe" to teacher
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>
    Co-authored-by: TibbersEvolved <erik.olin.spanga@gmail.com>

--- frontend/src/teacher-page/teacher-container.tsx
+++ frontend/src/teacher-page/teacher-container.tsx
@@ -7,6 +7,7 @@ export const Teacher = () => {
   const [selectedBootcamp, setSelectedBootcamp] = useState("");
   const [showManageBootcamp, setShowManageBootcamp] = useState(false);
   const [showAddBootcamp, setShowAddBootcamp] = useState(false);
+  const [showManageStudent, setShowMAnageStudent] = useState(false);
 
   const { user } = useUser();
 
@@ -26,12 +27,20 @@ export const Teacher = () => {
     }
   };
 
+  const handleShowManageStudent = () => {
+    setShowMAnageStudent(true);
+  };
+
   const closeManageBootcamp = () => {
     setShowManageBootcamp(false);
   };
 
   const closeAddBootcamp = () => {
     setShowAddBootcamp(false);
+  };
+
+  const closeShowManageStudent = () => {
+    setShowMAnageStudent(false);
   };
 
   return (
@@ -60,6 +69,14 @@ export const Teacher = () => {
         >
           Add a Bootcamp
         </button>
+        <div>
+          <button
+            className="mt-10 ml-8 pr-4 pl-4 text-center text-black bg-[#ebebeb] border border-3 border-black rounded-md"
+            onClick={handleShowManageStudent}
+          >
+            Handle students
+          </button>
+        </div>
       </div>
 
       {showManageBootcamp && (
@@ -77,6 +94,27 @@ export const Teacher = () => {
             <button
               className="mt-4 px-4 py-2 bg-black text-white rounded-md"
               onClick={closeAddBootcamp}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      )}
+
+      {showManageStudent && (
+        <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50">
+          <div className="bg-white p-6 rounded-md shadow-md w-5/6 h-5/6">
+            <h1>Manage Student</h1>
+            <ul>
+              <li>Lisa</li>
+              <li>Clara</li>
+              <li>Benny</li>
+              <li>Ptheven</li>
+            </ul>
+
+            <button
+              className="mt-4 px-4 py-2 bg-black text-white rounded-md"
+              onClick={closeShowManageStudent}
             >
               Close
             </button>
