1a9331b44424841de306fd4a78035f9764864218
AndreasErikLundmark <andreas.erik.lundmark@gmail.com>
2024-12-02 10:49:20 +0100

    gpt url
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Erik Olin <erik.olin.spanga@gmail.com>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>

--- backend/SaltCompanion/src/main/java/chilis/dev/SaltCompanion/controllers/GptController.java
+++ backend/SaltCompanion/src/main/java/chilis/dev/SaltCompanion/controllers/GptController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@RestController
+@RestController("/api")
 public class GptController {
 
     @Qualifier("openaiRestTemplate")
