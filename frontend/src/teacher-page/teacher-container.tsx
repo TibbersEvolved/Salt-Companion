67982e5e3503b1fb4e0fe002c29d4a7b8e0040b4
John Eliasson <john.eliasson@appliedtechnology.se>
2024-12-02 13:55:45 +0100

    Add clerk id
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>
    Co-authored-by: TibbersEvolved <erik.olin.spanga@gmail.com>

--- frontend/src/teacher-page/teacher-container.tsx
+++ frontend/src/teacher-page/teacher-container.tsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import { CreateBootCamp } from "./add-bootcamp";
+import { useUser } from "@clerk/clerk-react";
 
 export const Teacher = () => {
   const [selectedBootcamp, setSelectedBootcamp] = useState("");
   const [showSelectedBootcamp, setShowSelectedBootcamp] = useState(false);
   const [showAddBootcamp, setShowAddBootcamp] = useState(false);
+
+  const { user, isLoaded } = useUser();
 
   const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedBootcamp(e.target.value);
@@ -81,7 +84,7 @@ export const Teacher = () => {
         <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50">
           <div className="bg-white p-6 rounded-md shadow-md w-5/6 h-5/6">
             <h1>Add Bootcamp</h1>
-            <CreateBootCamp />
+            <CreateBootCamp clerkId={user?.id} />
             <button
               className="mt-4 px-4 py-2 bg-black text-white rounded-md"
               onClick={closeAddBootcamp}
