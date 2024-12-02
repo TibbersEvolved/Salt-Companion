2f6013baf2d9adba1bc8c21b3b7df9db6125215c
TibbersEvolved <erik.olin.spanga@gmail.com>
2024-12-02 15:47:47 +0100

    Fixed typo in dto
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>

--- backend/SaltCompanion/src/main/java/chilis/dev/SaltCompanion/controllers/dto/StudentDetailedInfoDto.java
+++ backend/SaltCompanion/src/main/java/chilis/dev/SaltCompanion/controllers/dto/StudentDetailedInfoDto.java
@@ -1,4 +1,4 @@
 package chilis.dev.SaltCompanion.controllers.dto;
 
-public record StudentDetailedInfoDto(String name, String bootcamp, ListDetailedTopicsDto topics, int streak, int currentStrek, int totalCardsFlipped) {
+public record StudentDetailedInfoDto(String name, String bootcamp, ListDetailedTopicsDto topics, int streak, int currentStreak, int totalCardsFlipped) {
 }
