25be09fc00426404fc5a9f3f6b82790d44c6fd31
Sabine <sabinehernandes@gmail.com>
2024-12-03 11:30:39 +0100

    front - make bar chart width responsive

--- frontend/src/landing-page/topics-chart.tsx
+++ frontend/src/landing-page/topics-chart.tsx
@@ -3,35 +3,35 @@ import { cheerfulFiestaPalette } from "@mui/x-charts/colorPalettes";
 
 export default function TopicsChart() {
   return (
-    <BarChart
-      xAxis={[
-        {
-          id: "barCategories",
-          data: [
-            "React",
-            "Typescript",
-            "Cloud",
-            "SpringBoot",
-            "Java",
-            "JavaScript",
-            "Database",
-            "SQL",
-            "HTML/CSS",
-            "git",
-          ],
-          scaleType: "band",
-        },
-      ]}
-      series={[
-        {
-          data: [2, 5, 3, 8, 4, 6, 7, 9, 10, 1],
-        },
-      ]}
-      className="bg-slate-200"
-      width={812}
-      height={300}
-      borderRadius={8}
-      // colors={cheerfulFiestaPalette}
-    />
+    <div className="w-full max-w-4xl m-auto">
+      <BarChart
+        xAxis={[
+          {
+            id: "barCategories",
+            data: [
+              "React",
+              "Typescript",
+              "Cloud",
+              "SpringBoot",
+              "Java",
+              "JavaScript",
+              "Database",
+              "SQL",
+              "HTML/CSS",
+              "git",
+            ],
+            scaleType: "band",
+          },
+        ]}
+        series={[
+          {
+            data: [2, 5, 3, 8, 4, 6, 7, 9, 10, 1],
+          },
+        ]}
+        height={300}
+        borderRadius={8}
+        // colors={cheerfulFiestaPalette}
+      />
+    </div>
   );
 }
