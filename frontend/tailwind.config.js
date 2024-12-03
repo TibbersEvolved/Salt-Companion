58ce40450d43f97ff98e4afb53c66c3e5310b455
Sabine <sabinehernandes@gmail.com>
2024-12-03 11:06:40 +0100

    front - install flowbite dependencies

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from "daisyui";
+const flowbite = require("flowbite-react/tailwind");
 export default {
-  content: ["./src/**/*.{html,tsx}"],
+  content: ["./src/**/*.{html,tsx}", flowbite.content()],
   theme: {
     extend: {},
   },
-  plugins: [daisyui],
+  plugins: [daisyui, flowbite.plugin()],
 };
