964acae7d3f5aa7c16ba27e14045d7d1a7062b5f
TibbersEvolved <erik.olin.spanga@gmail.com>
2024-12-03 16:24:42 +0100

    Revert of settings
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from "daisyui";
-import flowbite from "flowbite-react/tailwind";
+const flowbite = require("flowbite-react/tailwind");
 export default {
   content: ["./src/**/*.{html,tsx}", flowbite.content()],
   theme: {
