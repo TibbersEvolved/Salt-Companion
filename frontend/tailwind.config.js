8fdbb065316f946468e4fe95e0213647a3c28d59
TibbersEvolved <erik.olin.spanga@gmail.com>
2024-12-03 16:33:06 +0100

    Updated config
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 import daisyui from "daisyui";
 const flowbite = require("flowbite-react/tailwind");
 export default {
