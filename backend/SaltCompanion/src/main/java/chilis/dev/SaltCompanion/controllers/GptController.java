4f10413d190decc21647606d3235de4bd525d37e
AndreasErikLundmark <andreas.erik.lundmark@gmail.com>
2024-12-04 14:20:26 +0100

    cross origin
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Erik Olin <erik.olin.spanga@gmail.com>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>

--- backend/SaltCompanion/src/main/java/chilis/dev/SaltCompanion/controllers/GptController.java
+++ backend/SaltCompanion/src/main/java/chilis/dev/SaltCompanion/controllers/GptController.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@CrossOrigin
 @RestController("/api")
 @Tag(name = "GPT API", description = "API to interact with ChatGPT for generating responses")
 public class GptController {
