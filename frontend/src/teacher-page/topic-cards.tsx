cb45a761b94cdebf24b7cc6817c0127f5ba3b8be
AndreasErikLundmark <andreas.erik.lundmark@gmail.com>
2024-12-04 13:58:17 +0100

    button for update
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Erik Olin <erik.olin.spanga@gmail.com>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>

--- frontend/src/teacher-page/topic-cards.tsx
+++ frontend/src/teacher-page/topic-cards.tsx
@@ -75,8 +75,27 @@ export default function TopicCards({ topicId }: Props) {
   };
 
   return (
-    <div className="card-container">
-      <button onClick={updateHandler}>Update edited cards </button>
+    <div className="card-container relative">
+      <button
+        className="btn absolute right-5 bg-transparent border-none text-blue-500 hover:text-blue-700 cursor-pointer"
+        onClick={updateHandler}
+      >
+        Update/save cards{" "}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125"
+          />
+        </svg>{" "}
+      </button>
       <div className="overflow-x-auto">
         <table className="table w-full ">
           <thead>
