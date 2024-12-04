4b593bbfc0bd00a6006861007a730e84f881302c
Sabine <sabinehernandes@gmail.com>
2024-12-04 11:24:17 +0100

    front - change loading spinner colors and font weight git commit -m “$1
    Co-authored-by: Sabine Hernandes <sabine.hernandes@appliedtechnology.se>
    Co-authored-by: John Eliasson <john.eliasson@appliedtechnology.se>
    Co-authored-by: Andreas Lundmark <andreas.erik.lundmark@gmail.com>
    Co-authored-by: TibbersEvolved <erik.olin.spanga@gmail.com>

--- frontend/src/services/loadingScreen.tsx
+++ frontend/src/services/loadingScreen.tsx
@@ -1,13 +1,16 @@
 import LoadingSpinner from "../shared/loadingSpinner";
 
 export default function LoadingScreen(prop: props) {
-    return (<><div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4 text-2xl">
-        <div>{prop.displayText}</div><LoadingSpinner /></div>
-
-    </>)
+  return (
+    <>
+      <div className="min-h-screen bg-gray-100 font-light space-y-8 flex flex-col items-center justify-center p-4 text-2xl">
+        <div>{prop.displayText}</div>
+        <LoadingSpinner />
+      </div>
+    </>
+  );
 }
-
 
 type props = {
-    displayText: string;
-}
+  displayText: string;
+};
