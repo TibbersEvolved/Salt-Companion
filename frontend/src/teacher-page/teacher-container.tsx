a6d97b5795e5be1e2deccbe59db968af8b3adb2c
Sabine <sabinehernandes@gmail.com>
2024-12-05 15:08:58 +0100

    front - style closing button on student management

--- frontend/src/teacher-page/teacher-container.tsx
+++ frontend/src/teacher-page/teacher-container.tsx
@@ -98,15 +98,28 @@ export const Teacher = () => {
 
           {showManageStudent && (
             <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50">
-              <div className="bg-white p-6 rounded-md shadow-md w-5/6 h-5/6">
-                <Students bootCampId={selectedBootcampId} />
-                <br />
+              <div className="relative bg-white p-10 rounded-md shadow-md w-5/6 h-fit">
                 <button
-                  className="px-4 py-2 bg-black text-white rounded-md mt-auto mb-auto"
+                  className="absolute top-8 right-8 btn btn-circle bg-white hover:bg-[#f35b7e] hover:text-white"
                   onClick={closeShowManageStudent}
                 >
-                  Close
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-4 w-4"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
                 </button>
+                <Students bootCampId={selectedBootcampId} />
+                <br />
               </div>
             </div>
           )}
